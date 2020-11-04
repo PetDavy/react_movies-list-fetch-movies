62df280bf0f6b81ae43659329bc8375103b1844e
Petro <petdavy@1991gmail.com>
2020-11-04 21:17:21 +0200

    Updates

---
+++ precache-manifest.372f327355f42ab6842a7ba5d17b348c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75dba6a9c0b74f849525fd017823b60",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2b310f69039075dd1cb",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "59453177f0978bd4833a",
+    "url": "./static/css/main.92a3f45c.chunk.css"
+  },
+  {
+    "revision": "d2b310f69039075dd1cb",
+    "url": "./static/js/2.0357ca7b.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.0357ca7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59453177f0978bd4833a",
+    "url": "./static/js/main.a707c987.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
